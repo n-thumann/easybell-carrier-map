10774a20f2df32031bc76005ab348ac0c489155d
nthumann <me@n-thumann.de>
2023-02-04 23:29:49 +0100

    Add configuration

---
+++ src/config.ts
@@ -0,0 +1,21 @@
+import { Carrier } from "./models/carrier";
+
+const selectionWarningThreshold = 6_000_000;
+const concurrency = 6;
+const hexagonCellSide = 100;
+const minimumSelectionZoomLevel = 13;
+const proxies = ["https://corsproxy.io/?"];
+const carriers: { [key: string]: Carrier } = {
+  DTAG: new Carrier("Deutsche Telekom AG", "#e20074"),
+  "1U1": new Carrier("1&1", "#003d8f"),
+  VCN: new Carrier("Vitroconnect", "#009fe3"),
+};
+
+export {
+  selectionWarningThreshold,
+  concurrency,
+  hexagonCellSide,
+  minimumSelectionZoomLevel,
+  proxies,
+  carriers,
+};
